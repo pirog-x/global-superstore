616dfea2d1ef472257af735d716b13b2ff34d096
Vitalik <vitalikvlasenko88@gmail.com>
2023-01-19 10:49:58 +0200

    add PostRequest to /submitItem

---
+++ src/main/java/com/ltp/globalsuperstore/pojoClases/Item.java
@@ -0,0 +1,65 @@
+package com.ltp.globalsuperstore.pojoClases;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Item {
+    private String category;
+    private String name;
+    private Double price;
+    private Double discount;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public Item() {
+    }
+
+    public Item(String category, String name, Double price, Double discount, Date date) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
