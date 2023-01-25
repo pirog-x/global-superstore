728024a1bc0764213cced5156feb4c91383d18dd
Vitalik <vitalikvlasenko88@gmail.com>
2023-01-25 11:54:07 +0200

    add field validation

--- src/main/java/com/ltp/globalsuperstore/controllers/FormController.java
+++ src/main/java/com/ltp/globalsuperstore/controllers/FormController.java
@@ -4,11 +4,14 @@ import com.ltp.globalsuperstore.Constants;
 import com.ltp.globalsuperstore.pojoClases.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +27,6 @@ public class FormController {
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getItemIndex(id);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
         model.addAttribute("items", items);
         return "form";
     }
@@ -36,7 +38,13 @@ public class FormController {
     }
 
     @PostMapping("/submitItem")
-    public String submitItemHandler(Item item, RedirectAttributes redirectAttributes) {
+    public String submitItemHandler(@NotNull @Valid Item item, BindingResult result,
+                                    RedirectAttributes redirectAttributes) {
+        if (item.getPrice() < item.getDiscount()) {
+            result.rejectValue("price", "", "Price cannot be less than discount");
+        }
+        if (result.hasErrors()) return "form";
+
         int index = getItemIndex(item.getId());
 
         if (index == Constants.NOT_FOUND) {
